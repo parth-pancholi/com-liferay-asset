shuyangzhou/liferay-portal

LPS-75306 Implement in ADT.

modules/apps/web-experience/asset/asset-display-template-service/src/main/java/com/liferay/asset/display/template/internal/permission/AssetDisplayTemplatePermissionRegistrar.java
@@ -1,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.display.template.internal.permission;
+
+import com.liferay.asset.display.template.constants.AssetDisplayConstants;
+import com.liferay.asset.display.template.model.AssetDisplayTemplate;
+import com.liferay.asset.display.template.service.AssetDisplayTemplateLocalService;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true)
+public class AssetDisplayTemplatePermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put(
+			"model.class.name", AssetDisplayTemplate.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				AssetDisplayTemplate.class,
+				AssetDisplayTemplate::getAssetDisplayTemplateId,
+				_assetDisplayTemplateLocalService::getAssetDisplayTemplate,
+				_portletResourcePermission,
+				(modelResourcePermission, logicConsumer) -> {
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference
+	private AssetDisplayTemplateLocalService _assetDisplayTemplateLocalService;
+
+	@Reference(
+		target = "(resource.name=" + AssetDisplayConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+}

parth-pancholi/com-liferay-asset

asset-display-template-service/src/main/java/com/liferay/asset/display/template/internal/security/permission/AssetDisplayTemplatePermissionRegistrar.java
@@ -1,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.display.template.internal.security.permission;
+
+import com.liferay.asset.display.template.constants.AssetDisplayConstants;
+import com.liferay.asset.display.template.model.AssetDisplayTemplate;
+import com.liferay.asset.display.template.service.AssetDisplayTemplateLocalService;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true)
+public class AssetDisplayTemplatePermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put(
+			"model.class.name", AssetDisplayTemplate.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				AssetDisplayTemplate.class,
+				AssetDisplayTemplate::getAssetDisplayTemplateId,
+				_assetDisplayTemplateLocalService::getAssetDisplayTemplate,
+				_portletResourcePermission,
+				(modelResourcePermission, consumer) -> {
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference
+	private AssetDisplayTemplateLocalService _assetDisplayTemplateLocalService;
+
+	@Reference(
+		target = "(resource.name=" + AssetDisplayConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+}